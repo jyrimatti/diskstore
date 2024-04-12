6f97c0a6e6179586a772d893ee14e6e13d6fd8a0
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-04-12 20:55:15 +0300

    stuff

---
+++ cmd/getset.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i dash --pure -I channel:nixos-23.11-small -p dash coreutils
+
+key="$1"
+getset="${2:-}"
+value="${5:-}"
+
+FILE="${HOMEKIT_SH_CACHE_DIR:-$HOME/.cache/homekit.sh}/$key"
+
+if [ ! -f "$FILE" ]; then
+  touch "$FILE"
+fi
+
+if [ "$getset" = "Set" ]; then
+  echo "$value" > "$FILE"
+else
+  cat "$FILE"
+fi
