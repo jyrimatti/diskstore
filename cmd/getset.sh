d8a21d0b9a246f9945ce9da873d45dad50b3c226
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-01-21 20:35:09 +0200

    update nix

--- cmd/getset.sh
+++ cmd/getset.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash --pure -I channel:nixos-23.11-small -p dash coreutils
+#! nix-shell -i dash --pure -I channel:nixos-24.11-small -p dash coreutils
 
 key="$1"
 getset="${2:-}"
